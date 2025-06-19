4c81e8cae69e89684be2377429a57a82cf83f1d9
bryxxlhc <123710747+bryxxlionheart@users.noreply.github.com>
2025-06-19 13:19:22 +0800

    Initial commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import { Button } from '@/Components/ui/button';
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <Button>Button</Button>
+</template>
