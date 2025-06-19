d604244fde2803324573086d9535644f42b9681b
bryxxlhc <123710747+bryxxlionheart@users.noreply.github.com>
2025-06-19 16:41:00 +0800

    Added form

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,9 +1,19 @@
-<script setup>
+<script>
 import { Head } from '@inertiajs/vue3';
-import { Button } from '@/Components/ui/button';
+import LoginForm from '@/components/LoginForm.vue'
+export default {
+    components: {
+        Head,
+        LoginForm
+    }
+};
 </script>
 
 <template>
     <Head title="Welcome" />
-    <Button>Button</Button>
+    <div class="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+        <div class="w-full max-w-sm md:max-w-3xl">
+            <LoginForm />
+        </div>
+    </div>
 </template>
